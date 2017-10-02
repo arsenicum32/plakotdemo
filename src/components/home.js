ae84e01a4d93aaf380e63c1fc7db1bc8640ea5e9
Arseniy <zombiehot@yandex.ru>
2017-10-02 09:14:26 +0300

    feat: add Home page component with CTA button

---
+++ src/components/home.js
@@ -0,0 +1,26 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import Header from './header';
+
+import {main, cell, type1 , full, button } from '../styles/theme.styl'
+
+const Home = ({ name, onNameChange }) =>
+  <div className={ main }>
+    <Header />
+    <div className={cell + " " + full} style={{height: '70vh'}} >
+      <p>На этой странице вы сможете сгенерировать себе красивый постер,
+      баннер, обложку для группы, аватарку, открытку
+      и любой другой графический контент! <b>в пару кликов</b>
+      </p>
+      <br/>
+      <Link className={button} to="gallery">мне интересно</Link>
+    </div>
+  </div>;
+
+Home.propTypes = {
+  name: PropTypes.string.isRequired,
+  onNameChange: PropTypes.func.isRequired
+};
+
+export default Home;
+
