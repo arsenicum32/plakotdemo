70e72804f80993c510f3b5b587ef13b5d343b906
Arseniy <zombiehot@yandex.ru>
2017-10-21 11:40:21 +0300

    feat: add email container: connect email state and send action

---
+++ src/containers/email.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import Email from '../components/email';
+import { changeEmail , sendEmail } from '../redux/actions/email';
+
+const mapStateToProps = state => ({
+  email: state.email.email ,
+  canSignUp: state.email.canSignUp ,
+  thanks: state.email.thanks
+})
+
+const mapDispatchToProps = dispatch => ({
+  onChange:  e=> dispatch(changeEmail(e.target.value)) ,
+  click: e=> dispatch(sendEmail(e.target.value))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Email);
+
