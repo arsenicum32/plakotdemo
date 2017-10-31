e16651e82add2ddf980cb27d495fe2a0089ad394
Arseniy <zombiehot@yandex.ru>
2017-10-31 14:10:52 +0300

    feat: add client-side routing: define all app routes

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route , Router, IndexRoute } from 'react-router';
+import Main from '../containers/main';
+import Test from '../components/test';
+import Gallery from '../components/gallery';
+import Create from '../containers/create';
+import Email from '../containers/email';
+import notFound from '../components/notFound';
+
+export default () => (
+  <Router>
+    <Route path="/" >
+      <IndexRoute component={Main} />
+      <Route path="gallery" component={Gallery} />
+      <Route path="create" component={Create} />
+      <Route path="mail" component={Email} />
+    </Route>
+    <Route path="*" component={notFound} />
+  </Router>
+)
+
