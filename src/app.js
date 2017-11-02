3b43c3b89d850eff23e1de4f26f418eebbf2e8d8
Arseniy <zombiehot@yandex.ru>
2017-11-02 14:40:58 +0300

    feat: bootstrap app: mount Provider and Router to DOM

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { Router, hashHistory } from 'react-router'
+import configureStore from './redux/store'
+import routes from './routes'
+
+import './styles/index.styl'
+
+const store = configureStore();
+
+ReactDOM.render((
+  <Provider store={store}>
+    <Router history={hashHistory} routes={ routes(store) } />
+  </Provider>
+), document.getElementById('app'));
+
